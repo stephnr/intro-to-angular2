58980c869a2fc0f62b87107edae2e7389c2ef8d3
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-04-21 10:13:12 -0400

    add footer and update LICENSE

--- src/app/common/components/appFooter.component.ts
+++ src/app/common/components/appFooter.component.ts
@@ -1,7 +1,24 @@
-import { Component } from 'angular2/core';
+/*=============================================>>>>>
+= REQUIRED MODULES =
+===============================================>>>>>*/
+
+import {Component} from 'angular2/core';
+import {RouterLink} from 'angular2/router';
+
+/*= End of REQUIRED MODULES =*/
+/*=============================================<<<<<*/
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: 'src/app/common/components/layout/appFooter.html'
+  selector:    'app-footer',
+  templateUrl: 'src/app/common/components/layout/appFooter.html',
+  directives:   [RouterLink]
 })
-export class AppFooter { }
+export class AppFooter {
+  public appName: string;
+  public date: Date;
+
+  constructor() {
+    this.appName = 'conduit';
+    this.date = new Date();
+  }
+}
