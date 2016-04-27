b9c4a0ec8dadea9728b4312690ea65db7f50bb6c
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-04-26 22:47:06 -0400

    documenting router outlet

--- src/app/common/components/conduitRouterOutlet.component.ts
+++ src/app/common/components/conduitRouterOutlet.component.ts
@@ -1,15 +1,38 @@
+/*=============================================>>>>>
+= MODULES =
+===============================================>>>>>*/
+
 import {Directive, ElementRef, DynamicComponentLoader, Attribute} from 'angular2/core';
 import {Router, RouterOutlet, ComponentInstruction} from 'angular2/router';
 
 import {APP_CONSTANTS} from '../constants/app.constants';
 
+/*= End of MODULES =*/
+/*=============================================<<<<<*/
+
 @Directive({
   selector: 'conduit-router-outlet'
 })
 export class ConduitRouterOutlet extends RouterOutlet {
+  /**
+   * Defines the parent router (if one exists)
+   * @type {Router}
+   */
   private parentRouter: Router;
-  private publicRoutes: any;
 
+  /**
+   * Map of routes and flags defining public/private access
+   * @type {Object}
+   */
+  private publicRoutes: Object;
+
+  /**
+   * ConduitRouter Constructor
+   * @param  {ElementRef}             _elementRef       a wrapper around the native element inside of the view
+   * @param  {DynamicComponentLoader} _loader           service for instantiating a component and attaching it to a view at a specified location
+   * @param  {Router}                 _parentRouter     class containing mapper for of urls to components
+   * @param  {[type]}                 nameAttr          name attribute value provided by view
+   */
   constructor(_elementRef: ElementRef, _loader: DynamicComponentLoader, _parentRouter: Router, @Attribute('name') nameAttr: string) {
     super(_elementRef, _loader, _parentRouter, nameAttr);
     this.parentRouter = _parentRouter;
@@ -21,8 +44,13 @@ export class ConduitRouterOutlet extends RouterOutlet {
     };
   }
 
+  /**
+   * Event called on router redirect before component loaded
+   * @param  {ComponentInstruction} nextInstruction     the route state for a single component
+   */
   activate(nextInstruction: ComponentInstruction) {
     const url: any = nextInstruction.urlPath.split('/').shift();
+
     if (!this.publicRoutes[url] && !window.localStorage.getItem(APP_CONSTANTS.jwtKey)) {
       this.parentRouter.navigate(['Login']);
     }
